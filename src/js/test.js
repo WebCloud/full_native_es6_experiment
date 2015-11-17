c91509d7e700abe973ee88c02a97a1ac154ba356
Vinicius Dallacqua <vinicius.dallacqua@bonniergaming.com>
2015-11-18 00:22:49 +0100

    Add tests
    Using QUnit

--- src/js/test.js
+++ src/js/test.js
@@ -1,6 +1,60 @@
 import Base from './Base';
+import Table from './Table';
 
-QUnit.test('Hey', function(assert) {
-  let base = new Base('a', 'b');
-  assert.ok( base.itemTemplate === 'a');
+let itemTemplate,
+    viewTemplate,
+    base,
+    table;
+
+itemTemplate = `
+  <tr>
+    <td><name></td>
+    <td><size></td>
+    <td><openLevel></td>
+    <td><level></td>
+    <td><profit></td>
+  </tr>
+`;
+viewTemplate = `
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Size</th>
+      <th>Open Level</th>
+      <th>Level</th>
+      <th>Profit/Loss</th>
+    <tr>
+  </thead>
+  <tbody>
+    <formattedItems>
+  </tbody>
+`;
+
+base = new Base(itemTemplate, viewTemplate);
+table = new Table(itemTemplate, viewTemplate);
+
+QUnit.test('base.getParsedViewData() returns data parsed against the itemTemplate', function(assert) {
+  assert.expect(1);
+  let done = assert.async();
+
+  base.getParsedViewData().then((data)=>{
+    assert.ok(data.match(`<td>${base.viewData[0].name}</td>`).length > 0);
+    done();
+  });
+});
+
+QUnit.test('table.render parses the viewData into the viewTemplate', function(assert) {
+  assert.expect(1);
+  let done = assert.async();
+
+  let hiddenTarget = document.createElement('table');
+  hiddenTarget.className = 'hidden-target';
+  hiddenTarget.style.display = 'none';
+  hiddenTarget.addEventListener('DOMSubtreeModified', ()=>{
+    assert.ok(document.querySelectorAll('.hidden-target th').length > 0);
+    done();
+  });
+  document.body.appendChild(hiddenTarget);
+
+  table.render('.hidden-target')();
 });
